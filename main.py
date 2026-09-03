3e93b36f1e3202fe5c9d31cd86dca432d9c9c873
Joshua Mutavi <alolagamerza@gmail.com>
2026-09-02 19:57:04 -0500

    Add core logic

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Face Expression Reader
+
+Reads a rough facial expression (neutral, smiling, surprised, eyes closed)
+from your webcam using face landmark geometry. No cloud APIs, no trained
+model to download -- everything runs locally off distances between points
+mediapipe already gives you.
+"""
